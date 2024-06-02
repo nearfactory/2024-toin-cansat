e1fcca24a640e4e91a51eedac7240d2afe89e842
maguronoosushi0807 <164456890+maguronoosushi0807@users.noreply.github.com>
2024-06-02 23:13:09 +0900

    デスクトップアプリのプログラムを追加した

---
+++ プログラム/CansatDesktop/src/hoge.cpp
@@ -0,0 +1,7 @@
+#include "hoge.h"
+
+void drawGraph(int array, int x, int y, int w, int h, int refreshRate) {
+	//double interval = w / (sizeof(array) / sizeof(array[0]));
+	//cout << interval;
+	return;
+};
---
+++ プログラム/CansatDesktop/src/hoge.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include"ofMain.h"
+
+void drawGraph(int array,int x,int y,int w,int h, int refreshRate); //left upper
