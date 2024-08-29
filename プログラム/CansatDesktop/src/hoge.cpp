ddcb3e49cb6ff5434caf5999b213dc5c832a79c6
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-08-30 00:10:18 +0900

    ファイル整理した

--- プログラム/CansatDesktop/src/hoge.cpp
+++
@@ -1,7 +0,0 @@
-#include "hoge.h"
-
-void drawGraph(int array, int x, int y, int w, int h, int refreshRate) {
-	//double interval = w / (sizeof(array) / sizeof(array[0]));
-	//cout << interval;
-	return;
-};
--- プログラム/CansatDesktop/src/hoge.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include"ofMain.h"
-
-void drawGraph(int array,int x,int y,int w,int h, int refreshRate); //left upper
